33b65f3547eaf531bb47529abd4380c9a1161d12
toabr <25079664+toabr@users.noreply.github.com>
2021-09-28 12:20:35 +0200

    added highlight.js to Post.js

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -1,12 +1,12 @@
 import { LinkContainer } from 'react-router-bootstrap'
-
 import { Button, Col, Container } from 'react-bootstrap'
 import PageTitle from '../components/PageTitle'
 import Meta from '../components/Meta'
 import useFetch from '../hooks/useFetch'
 import { urlBuilder, addTagTitles } from '../js/helper'
-
+import hljs from 'highlight.js'
 import './post.scss'
+
 
 /**
  * fetch one Post and spit it out
@@ -21,6 +21,7 @@ const Post = (props) => {
   const title = `Post ${nid}`
   let description = ''
   let isLoading = true // state of PageTitle
+  let body = ''
 
 
   const url = urlBuilder({ nid })
@@ -36,10 +37,36 @@ const Post = (props) => {
       description = node.title[0].value
       // add tag titles
       node.field_tags = addTagTitles(node.field_tags, tagList)
+      
+      body = highLight(node.body[0].value)
+      
     } else {
       description = 'nothing here'
     }
     isLoading = false
+  }
+
+  
+  /**
+   * Add syntax highlighting to the pre elements of the body
+   * @param {string} body - html of the body tag as a string
+   * @returns {string}
+   * TODO: read the classes of the pre,
+   * to determin the language to use
+   */
+  function highLight(body) {
+    const languages = ['sh','js','json','css','scss','docker','php','yml']
+    
+    const wrapper = document.createElement('div')
+    wrapper.innerHTML = body
+  
+    for(const pre of wrapper.getElementsByTagName('pre')) {
+      const lighted = hljs.highlightAuto(pre.innerText, languages)
+      console.log('hljs', lighted)
+      pre.classList.add('hljs', `lang-${lighted.language}`)
+      pre.innerHTML = lighted.value
+    }
+    return wrapper.outerHTML
   }
 
 
@@ -52,7 +79,7 @@ const Post = (props) => {
         <div id={`post-${nid}`} style={{ maxWidth: 685 }} className="post mx-auto px-3">
 
           {/* TODO: check dangerouslySetInnerHTML */}
-          <div dangerouslySetInnerHTML={{ __html: node.body[0].value }} />
+          <div dangerouslySetInnerHTML={{ __html: body }} />
 
           <div className="my-3">
             {node.field_tags.map(nodeTag => (
