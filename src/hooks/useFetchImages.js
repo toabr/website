09c00af929af1c93f1a751fc2e846e7a870e70d1
toabr <25079664+toabr@users.noreply.github.com>
2021-10-22 09:02:52 +0200

    image api change

--- src/hooks/useFetchImages.js
+++ src/hooks/useFetchImages.js
@@ -38,8 +38,8 @@ function useFetchImages({ nid, fids }) {
   let url = null
   const apiBase = `${process.env.REACT_APP_API_URL}/rest/v2/image`
 
-  if (nid) url = `${apiBase}?nid=${nid}`
-  if (fids) url = `${apiBase}/${fids.toString()}`
+  if (nid) url = `${apiBase}/${nid}`
+  if (fids) url = `${apiBase}/all/${fids.toString()}`
 
   const { status, data: images, error } = useFetch(url)
   // console.log('data', images)
