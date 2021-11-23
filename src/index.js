6e7b9f40d7256c91eb659190f6ac2259be856ca8
toabr <25079664+toabr@users.noreply.github.com>
2021-11-23 12:08:41 +0100

    reorganized styles

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './scss/bootstrap.scss';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import App from './App';
+import App from './App'
 
 
 ReactDOM.render(<App />, document.getElementById('root'))
